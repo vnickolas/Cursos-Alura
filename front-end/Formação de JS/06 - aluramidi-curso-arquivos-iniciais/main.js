5b43d0717c9c73771191fe0ac07e894bf1271228
vnickolas <vitornickolas10@gmail.com>
2024-03-22 15:02:54 -0300

    att

--- front-end/Formação de JS/06 - aluramidi-curso-arquivos-iniciais/main.js
+++ front-end/Formação de JS/06 - aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,13 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
 
-  document.querySelector(idElementoAudio).play()
+  const elemento = document.querySelector(seletorAudio) //coloca a id que foi puxada lá embaixo (ex: #som_tecla_tic) dentro da variavel 'elemento'
+     if (elemento && elemento.localName === 'audio') { //se essa id for igual a null E se o localName dessa ID (tagname como header/input/audio...) for igual a 'audio', ele reproduzirá a função abaixo
+        elemento.play()
+     }
+      else {
+        alert('Elemento não encontrado ou seletor inválido')
+      }
+
 
 }
 
@@ -25,13 +32,17 @@ function tocaSom (idElementoAudio) {
               console.log(`variavel ${idAudio}`) // APARECE COMO: variavel #som_tecla_tic
           
           tecla.onclick = function(){
-              tocaSom(idAudio); //colocamos o idAudio para ele puxar qual o nome exato da class que será tocada. Além disso, como estamos puxando a função 'tomaSom' lá de cima, colocamos entre os paranteses o 'idAudio' como parametro
+              tocaSom(idAudio); //colocamos o idAudio para ele puxar qual o nome exato da class que será tocada. Além disso, como estamos puxando a função 'tomaSom' lá de cima, escrevendo o 'idAudio' entre parenteses, ele entra lá em cima como parametro também.
 
           } 
 
-          tecla.onkeydown = function () {
-            // onjeydown é quando pelo tab, tecla do teclado está selecionada
-            tecla.classList.add(`ativa`); //adiciona uma classe (classList) e coloca o nome da classe entre parenteses
+          tecla.onkeydown = function (event) {
+            // onjeydown é quando pelo tab, a tecla do teclado está selecionada, logo...
+            // ...fará tal função. Colocamos como parâmetro o 'event' para a função, pois qualquer tecla da lista de teclas que estiverem selecionadas pelo tab (focadas em amarelo), caso a tecla que a pessoa abertar seja 'Space" ou 'Enter', será adicionado uma class 'ativa' e assim, será reproduzido o efeito do css 
+            if (event.code === 'Space' || event.code === 'Enter') { //o '.code' entra no "nome" da tecla apertada e como colocarmos para ver se é igual ao 'Space' ou 'Enter', ele fará a função abaixo:
+
+              tecla.classList.add(`ativa`); //adiciona uma classe (classList) e coloca o nome da classe entre parenteses
+            }
           }
 
           tecla.onkeyup = function () {
---
+++ front-end/Formação de JS/07 - Fokus-projeto-base/script.js
@@ -0,0 +1,48 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+
+focoBt.addEventListener('click', () => { // '() =>' é uma arrow function 
+    alterarContexto('foco')
+    focoBt.classList.add('active')
+})
+
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+})
+
+longoBt.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
+})
+
+//MUDANDO A FOTO E COR DE FUNDO DOS MODOS DE CONTEXTO
+function alterarContexto (contexto) {
+    botoes.forEach(function (contexto){ // Neste trecho de código, a função alterarContexto percorre todos os botões e remove a classe "active" de cada um deles, garantindo que apenas um botão fique ativo por vez.
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)// primeiro parametro é onde vc quer mexer (no 'data-contexto'), o segundo é para o que você quer mudar (para 'foco')
+    banner.setAttribute('src', `imagens/${contexto}.png`)
+
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = 
+            'Otimiza sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa</strong>'
+            break;
+
+        case 'descanso-curto':
+            titulo.innerHTML = 
+            'Que tal dar uma respirada?<br> <strong class="app__title-strong">Faça uma pausa curta</strong>'
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = 'Hora de voltar à superfície<br> <strong class="app__title-strong">Faça uma pausa longa</strong>'
+        default: 
+            break;
+    }
+}
+
